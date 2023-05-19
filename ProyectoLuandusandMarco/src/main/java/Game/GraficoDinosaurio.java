1a26082e20dff006f47fc6f3bb48d6e41f9e7411
MarcoMB352 <79714168+MarcoMB352@users.noreply.github.com>
2023-05-19 14:07:42 +0200

    Add files via upload

--- ProyectoLuandusandMarco/src/main/java/Game/GraficoDinosaurio.java
+++ ProyectoLuandusandMarco/src/main/java/Game/GraficoDinosaurio.java
@@ -5,6 +5,14 @@ import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 
 import javax.swing.JPanel;
 
@@ -14,6 +22,7 @@ import ObjetosDinosaurio.Dino;
 import ObjetosDinosaurio.Isla;
 import ObjetosDinosaurio.Nubes;
 import Util.Resource;
+import common.Logger;
 
 public class GraficoDinosaurio extends JPanel implements Runnable, KeyListener {
 	public static final int JUEGO_STATUS = 0;
@@ -28,10 +37,18 @@ public class GraficoDinosaurio extends JPanel implements Runnable, KeyListener {
 	private Nubes nubes;
 	private DieDino diedino;
 	private int puntos;
-<<<<<<< HEAD
 	private Dinosaurio dn;
 	private int statusGame = JUEGO_STATUS;
 	private boolean saltar = true;
+	
+	public Properties prop=null;
+	public FileInputStream is = null;
+	public Statement st=null;
+	public PreparedStatement stmt=null;
+	public ResultSet rs=null;
+	public String sql=null;
+	public Statement statement=null;
+	public Connection conn=null;
 
 	private BufferedImage imgGameOver;
 	private BufferedImage imgReplay;
@@ -109,6 +126,7 @@ public class GraficoDinosaurio extends JPanel implements Runnable, KeyListener {
 			dino.draw(g);
 			diedino.draw(g);
 			g.drawString("Puntos: " + String.valueOf(puntos), 500, 20);
+			insertarPuntuacion(puntos);
 			break;
 		case JUEGO_ACABA:
 			nubes.draw(g);
@@ -170,133 +188,46 @@ public class GraficoDinosaurio extends JPanel implements Runnable, KeyListener {
 				restartGame();
 				statusGame = JUEGO_EMPIEZA;
 				puntos = 0;
-=======
 
-	private int statusGame = JUEGO_STATUS;
-
-	private BufferedImage imgGameOver;
-	private BufferedImage imgReplay;
-	public GraficoDinosaurio() {
-		thread = new Thread(this);
-		dino = new Dino();
-		// posicion que inicia dinosaurio
-		dino.setX(50);
-		dino.setY(60);
-		isla = new Isla(this);
-		nubes = new Nubes();
-		diedino = new DieDino(dino,this);
-		imgGameOver = Resource.getResourceImage("./contenido/gameover_text.png");
-		imgReplay = Resource.getResourceImage("./contenido/replay_button.png");
-	}
-
-	public void empiezaJuego() {
-		thread.start();
-	}
-
-	@Override
-	public void run() {
-		while (true) {
-			try {
-				actualizar();
-				repaint();
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
 		}
 	}
-
-	public void actualizar() {
-		switch (statusGame) {
-		case JUEGO_EMPIEZA:
-			dino.actualizar();
-			isla.actualizar();
-			nubes.actualizar();
-			diedino.actualizar();
-			if (!dino.getVida()) {
-				statusGame = JUEGO_ACABA;
-			}
-			break;
-
+	public void conectar() {
+		try {
+			prop=new Properties();
+			is = new FileInputStream("src/main/resources/bd.properties");
+			prop.load(is);
+			String user = prop.getProperty("user","");
+			String password = prop.getProperty("password","");
+			String url = prop.getProperty("url","");
+			String driver = prop.getProperty("driver","");
+			Class.forName(driver).newInstance();
+			conn = DriverManager.getConnection(url, user, password);
+			System.out.println("== Conexion establecida ==");
+		} catch (SQLException e) {
+		e.printStackTrace();
+		} catch (Exception e) {
+		e.printStackTrace();
 		}
-
 	}
-	
-	public void plusPuntos(int puntos) {
-		this.puntos += puntos;
-	}
-
-	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
-		g.setColor(Color.decode("#f7f7f7"));
-		g.fillRect(0, 0, getWidth(), getHeight());
-//		g.setColor(Color.black);
-//		g.drawLine(0, (int) GROUNDY, getWidth(), (int) GROUNDY);
-
-		switch (statusGame) {
-		case JUEGO_STATUS:
-			dino.draw(g);
-			nubes.draw(g);
-			isla.draw(g);
-			dino.draw(g);
-			diedino.draw(g);
-			g.drawString("Puntos: " + String.valueOf(puntos), 500, 20);
-			break;
-		case JUEGO_EMPIEZA:
-			nubes.draw(g);
-			isla.draw(g);
-			dino.draw(g);
-			diedino.draw(g);
-			g.drawString("Puntos: " + String.valueOf(puntos), 500, 20);
-			break;
-		case JUEGO_ACABA:
-			nubes.draw(g);
-			isla.draw(g);
-			dino.draw(g);
-			diedino.draw(g);
-			if (statusGame == JUEGO_ACABA) {
-				g.drawImage(imgGameOver, 180, 40, null);
-				g.drawImage(imgReplay, 260, 60, null);
+	public void insertarPuntuacion(int puntuacion) {
+		try {
+			Statement st = conn.createStatement();
+			String sql="SELECT nom_usuario FROM USUARIOS ORDER BY id DESC LIMIT 1";
+			ResultSet rs = st.executeQuery(sql);
+			while(rs.next()) {
+				String nom_usuario=rs.getString(1);
+				PreparedStatement statement=conn.prepareStatement("UPDATE USUARIOS SET nom_usuario=?, puntuacion=? WHERE nom_usuario='"+nom_usuario+"'");
+				statement.setString(1,nom_usuario);
+				statement.setInt(2,puntuacion);
+				int retorno=statement.executeUpdate();
+				if (retorno>0) {
+				}
 			}
-			break;
-
-		}
-
-	}
-	
-	private void restartGame() {
-		dino.setVida(true);
-		dino.setX(50);
-		dino.setY(60);
-		diedino.reset();
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		switch (e.getKeyCode()) {
-		case KeyEvent.VK_SPACE:
-			if (statusGame == JUEGO_STATUS) {
-				statusGame = JUEGO_EMPIEZA;
-			} else if (statusGame == JUEGO_EMPIEZA) {
-				dino.jump();
-			} else if (statusGame == JUEGO_ACABA) {
-				restartGame();
-				statusGame = JUEGO_STATUS;
->>>>>>> branch 'master' of https://github.com/LuandusW/Proyecto
-			}
-			break;
+		} catch (SQLException e) {
+		e.printStackTrace();
+		} catch (Exception e) {
+		e.printStackTrace();
 		}
 	}
 
